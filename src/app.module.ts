359d8abbff8d3117d9793cb6efa9f753c228d8f0
racibaz <r.c67@hotmail.com>
2024-12-10 18:49:30 +0300

    config module is added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,9 +7,26 @@ import { PostsModule } from './posts/application/posts.module';
 import { CoreModule } from './core/core.module';
 import { ApplicationBootstrapOptions } from './posts/common/interface/application-bootstap-options';
 import { PostsInfrastructureModule } from './posts/infrastructre/posts-infrastructure.module';
+import { ConfigModule } from '@nestjs/config';
+
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import environmentValidation from './config/environment.validation';
+
+// Get the current NODE_ENV
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [CoreModule],
+  imports: [
+    CoreModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      //envFilePath: ['.env.development', '.env'],
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [appConfig, databaseConfig],
+      validationSchema: environmentValidation,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
