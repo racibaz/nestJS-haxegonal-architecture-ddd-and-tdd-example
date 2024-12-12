975ab70e45814ff0bc3757c1efd0651697c76423
raci <r.c67@hotmail.com>
2024-12-12 10:31:04 +0300

    posts dtos class validation implemented.

--- src/posts/presenters/http/dto/create-post.dto.ts
+++ src/posts/presenters/http/dto/create-post.dto.ts
@@ -1,4 +1,12 @@
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreatePostDto {
+  @MinLength(5)
+  @MaxLength(25)
+  @IsString()
   public readonly title: string;
+
+  @MinLength(10)
+  @IsString()
   public readonly description: string;
 }
