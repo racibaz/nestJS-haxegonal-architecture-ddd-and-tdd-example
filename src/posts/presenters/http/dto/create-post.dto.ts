e86ccdffb1f5cb8abe08ffc55ce898cf32f5f3b3
raci <r.c67@hotmail.com>
2024-12-12 10:35:59 +0300

    posts dtos updated.

--- src/posts/presenters/http/dto/create-post.dto.ts
+++ src/posts/presenters/http/dto/create-post.dto.ts
@@ -1,12 +1,14 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreatePostDto {
   @MinLength(5)
   @MaxLength(25)
+  @IsNotEmpty()
   @IsString()
   public readonly title: string;
 
   @MinLength(10)
+  @IsNotEmpty()
   @IsString()
   public readonly description: string;
 }
