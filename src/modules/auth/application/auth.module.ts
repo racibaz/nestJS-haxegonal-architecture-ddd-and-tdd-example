58514b369cdf905c259f35fee0129c1bbd2a9c2a
raci <r.c67@hotmail.com>
2024-12-19 11:16:09 +0300

    OrmAuthPersistenceModule from export is removed.

--- src/modules/auth/application/auth.module.ts
+++ src/modules/auth/application/auth.module.ts
@@ -6,7 +6,7 @@ import { User } from '../../users/domain/user';
 import { OrmAuthPersistenceModule } from '../infrastructure/orm-persistence.module';
 import { UserMapper } from '../../users/infrastructure/persistence/orm/mappers/user.mapper';
 import { HashingProvider } from './ports/hashing.provider';
-import { BcryptProvider } from '../infrastructure/persistence/hashing/bcrypt.provider';
+import { BcryptProvider } from '../infrastructure/hashing/bcrypt.provider';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import jwtConfig from '../config/jwt.config';
@@ -28,6 +28,6 @@ import jwtConfig from '../config/jwt.config';
     User,
     UserMapper,
   ],
-  exports: [OrmAuthPersistenceModule],
+  exports: [],
 })
 export class AuthModule {}
